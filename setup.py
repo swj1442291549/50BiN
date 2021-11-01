f53fadab0f826348c1a4b0364f57c7937623cc77
Weijia Sun <swj1442291549@gmail.com>
2021-11-01 09:26:57 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="50bin",
-    version="0.1.1",
+    version="0.1.2",
     py_modules=["catmerge", "plotcurve"],
     install_requires=["Click", "astropy", "pandas", "numpy", "matplotlib", "tqdm"],
     entry_points={
