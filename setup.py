3c350d11ab406629a8fdd651f373516b458534e8
Weijia Sun <swj1442291549@gmail.com>
2021-11-05 08:33:40 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name="50bin",
     version="0.2.0",
-    py_modules=["catmerge", "plotcurve"],
+    py_modules=["catmerge", "correctphot", "plotcurve"],
     install_requires=["Click", "astropy", "pandas", "numpy", "matplotlib", "tqdm"],
     entry_points={
-        "console_scripts": ["catmerge = catmerge:cli", "plotcurve = plotcurve:cli"],
+        "console_scripts": ["catmerge = catmerge:cli", "plotcurve = plotcurve:cli", "correctphot = correctphot:cli"],
     },
 )
