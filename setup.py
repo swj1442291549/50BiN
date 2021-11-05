89bec73d890892ece5da2e8ac870671b123723f6
Weijia Sun <swj1442291549@gmail.com>
2021-11-05 08:34:20 +0800

    Rename catmerge to mergecat

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name="50bin",
     version="0.2.0",
-    py_modules=["catmerge", "correctphot", "plotcurve"],
+    py_modules=["mergecat", "correctphot", "plotcurve"],
     install_requires=["Click", "astropy", "pandas", "numpy", "matplotlib", "tqdm"],
     entry_points={
-        "console_scripts": ["catmerge = catmerge:cli", "plotcurve = plotcurve:cli", "correctphot = correctphot:cli"],
+        "console_scripts": ["mergecat = mergecat:cli", "plotcurve = plotcurve:cli", "correctphot = correctphot:cli"],
     },
 )
