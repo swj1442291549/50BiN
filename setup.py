e4dad28e3ca6da4d86f5f1312592b169cc133f05
Weijia Sun <swj1442291549@gmail.com>
2021-11-05 15:39:30 +0800

    Update version 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="50bin",
-    version="0.2.0",
+    version="0.3.0",
     py_modules=["mergecat", "correctphot", "plotcurve"],
     install_requires=["Click", "astropy", "pandas", "numpy", "matplotlib", "tqdm"],
     entry_points={
