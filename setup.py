3dad8b43e4b1eebc8b486337e31d913dd7f75e4e
Weijia Sun <swj1442291549@gmail.com>
2021-11-25 16:25:38 +0800

    Update to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="50bin",
-    version="0.4.0",
+    version="0.4.1",
     py_modules=["mergecat", "correctphot", "plotcurve"],
     install_requires=[
         "Click",
